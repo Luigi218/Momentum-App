be14078d5a05c575e16911ed6ca5681c93a2e48c
Zeak218 <iggy.torres218@gmail.com>
2023-03-15 18:32:01 +0800

    commit message

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
-setInterval(myTimer, 1000);
-
+//Real-Time Clock Function
 function myTimer (){
-    const d = new Date();
-    document.getElementById("clock").innerHTML = d.toLocaleTimeString();
+    const date = new Date();
+    document.getElementById("clock").innerHTML = date.toLocaleTimeString();
+} 
+
+//Update every second
+setInterval(myTimer,1000)
+
+//Background Images Array
+const backgroundImages = [
+    "url(assets/img-1.jpg)", 
+    "url(assets/img-2.jpg)", 
+    "url(assets/img-3.jpg)"
+]
+
+const background = document.querySelector("body")
+
+//Changing Background Function
+function changeBackground () {
+        let i = 0
+        background.style.backgroundImage = backgroundImages[i]
+        background.style.backgroundRepeat = "no-repeat"
+        background.style.backgroundSize = "cover"
 }
 
-
+setInterval(changeBackground, 1000)
