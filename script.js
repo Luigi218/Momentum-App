97c8a330eda6619d42b4b9602f46c518b84df88c
Zeak218 <iggy.torres@yahoo.com>
2023-03-16 16:44:47 +0800

    commit message

--- script.js
+++ script.js
@@ -27,5 +27,42 @@ function changeBackground () {
         background.style.backgroundSize = "cover"
 }
 
+//Load in Background Image
+window.onload = (
+    background.style.backgroundImage = backgroundImages[0],
+    background.style.backgroundRepeat = "no-repeat",
+    background.style.backgroundSize = "cover"
+)
+
 //Update Background every minute
 setInterval(changeBackground, 60000)
+
+//Quotes Array
+const quotesList = [
+    {
+        quote: "Words are not meant to sir the air only: they are capable of moving greater things",
+        author: "Natsume Soseki"
+    },
+    {
+        quote: "I think that inside every adult is the heart of a child. We just gradually convince ourselves that we whave to act more like adults",
+        author: "Shigeru Miyamoto"
+    },
+    {
+        quote: "The objective of clearning is not just to clean, but to feel happiness living within that environment",
+        author: "Marie Kondo"
+    }
+]
+
+//Start Index at 0
+let i = 0
+const quoteText = document.getElementById("quote-text")
+const authorText = document.getElementById("author-text")
+
+//Change Quote Function
+function changeQuote () {
+    i++
+    if (i === quotesList.length) {i = 0}
+    
+}
+
+setInterval(changeQuote, 5000)
